4d6c0e00c5f7b18041dd58ed37cad5f4754b0b9a
Janik Hotz <snangles.void@gmail.com>
2019-01-15 21:25:42 +0100

    sdfkljfds

--- routes/views/contact.js
+++ routes/views/contact.js
@@ -27,6 +27,27 @@ exports = module.exports = function (req, res) {
 			response: token
 		});
 
+		var post_options = {
+			host: 'https://www.google.com',
+			port: '80',
+			path: '/recaptcha/api/siteverify',
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				'Content-Length': Buffer.byteLength(post_data)
+			}
+		};
+
+		var post_req = http.request(post_options, function(res) {
+			res.setEncoding('utf8');
+			res.on('data', function (chunk) {
+				console.log('Response: ' + chunk);
+			});
+		});
+	  
+		// post the data
+		post_req.write(post_data);
+		post_req.end();
 
 
 		var newEnquiry = new Enquiry.model();
